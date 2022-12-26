0867aba9072eba5854c3c7c6368816674e469494
rodrigoCode-tech <rodrigobraga0125@gmail.com>
2022-12-26 18:14:57 -0300

    Criação dos metodos GetAll e GetOne

--- src/main/java/com/api/parkingcontrol/controller/parkingSpotController.java
+++ src/main/java/com/api/parkingcontrol/controller/parkingSpotController.java
@@ -2,6 +2,9 @@ package com.api.parkingcontrol.controller;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 import javax.validation.Valid;
 
@@ -11,6 +14,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -53,4 +58,19 @@ public class parkingSpotController {
         
         return ResponseEntity.status(HttpStatus.CREATED).body(service.save(parkingSpotModel));
 	}
+	
+	@GetMapping
+	public ResponseEntity<List<ParkingSpotModel>>getAllParkingSpots(){
+		return ResponseEntity.status(HttpStatus.OK).body(service.findAll());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Object> getOneParkingSpot(@PathVariable(value = "id") UUID id) {
+		Optional<ParkingSpotModel> parkingSpotModelOptional = service.findById(id);
+		if (!parkingSpotModelOptional.isPresent()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Parking Spot not found.");
+		}
+		return ResponseEntity.status(HttpStatus.OK).body(parkingSpotModelOptional.get());
+	}
+	
 }
--- src/main/java/com/api/parkingcontrol/service/parkingSpotService.java
+++ src/main/java/com/api/parkingcontrol/service/parkingSpotService.java
@@ -1,5 +1,9 @@
 package com.api.parkingcontrol.service;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,6 +36,15 @@ public class parkingSpotService {
 
 	public  boolean existsByApartmentAndBlock(String apartment, String block) {
 		return repository.existsByApartmentAndBlock(apartment, block);
+	}
+
+	public List<ParkingSpotModel> findAll() {
+		return repository.findAll();
+	}
+
+	public Optional<ParkingSpotModel> findById(UUID id) {
+		
+		return repository.findById(id);
 	}
 	
 	
