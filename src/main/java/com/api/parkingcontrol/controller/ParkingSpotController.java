656f11b28d597d91db065ff220acfa25d07c2e4c
Marcio Rodrigo <rodrigobraga0125@gmail.com>
2023-11-25 12:08:53 -0300

    adjusts service duplicate

--- src/main/java/com/api/parkingcontrol/controller/ParkingSpotController.java
+++ src/main/java/com/api/parkingcontrol/controller/ParkingSpotController.java
@@ -34,27 +34,21 @@ import com.api.parkingcontrol.service.ParkingSpotService;
 @RequestMapping("/parkingSpot")
 public class ParkingSpotController {
 
-	@Autowired
-	private ParkingSpotService service;
+	private final ParkingSpotService service;
+
+	public ParkingSpotController(ParkingSpotService service) {
+		this.service = service;
+	}
 
 	@PostMapping
     public ResponseEntity<Object> saveParkingSpot(@RequestBody @Valid ParkingSpotDto parkingSpotDto){
 		ParkingSpotModel parkingSpotModel = new ParkingSpotModel();
 
-		if(service.existsByLicensePlateCar(parkingSpotDto.getLicensePlateCar())){
-            return ResponseEntity.status(HttpStatus.CONFLICT).body("Conflict: License Plate Car is already in use!");
-        }
-        if(service.existsByParkingSpotNumber(parkingSpotDto.getParkingSpotNumber())){
-            return ResponseEntity.status(HttpStatus.CONFLICT).body("Conflict: Parking Spot is already in use!");
-        }
-
-        if(service.existsByApartmentAndBlock(parkingSpotDto.getApartment(), parkingSpotDto.getBlock())){
-            return ResponseEntity.status(HttpStatus.CONFLICT).body("Conflict: Parking Spot already registered for this apartment/block!");
-        }
-		
+		if (service.existsDuplicateParkingSpot(parkingSpotDto)) {
+			return ResponseEntity.status(HttpStatus.CONFLICT).body("Conflict: Duplicate parking spot information!");
+		}
 
         BeanUtils.copyProperties(parkingSpotDto, parkingSpotModel);
-
         parkingSpotModel.setRegistrationDate(LocalDateTime.now(ZoneId.of("UTC")));
         
         return ResponseEntity.status(HttpStatus.CREATED).body(service.save(parkingSpotModel));
@@ -72,8 +66,11 @@ public class ParkingSpotController {
 	@GetMapping("/{id}")
 	public ResponseEntity<Object> getOneParkingSpot(@PathVariable(value = "id") UUID id) {
 		Optional<ParkingSpotModel> parkingSpotModelOptional = service.findById(id);
-        return parkingSpotModelOptional.<ResponseEntity<Object>>map(parkingSpotModel -> ResponseEntity.status(HttpStatus.OK).body(parkingSpotModel)).orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).body("Parking Spot not found."));
-    }
+		if (!parkingSpotModelOptional.isPresent()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Parking Spot not found.");
+		}
+		return ResponseEntity.status(HttpStatus.OK).body(parkingSpotModelOptional.get());
+	}
 	
 	@DeleteMapping("/{id}")
     public ResponseEntity<Object> deleteParkingSpot(@PathVariable(value = "id") UUID id){
@@ -95,7 +92,7 @@ public class ParkingSpotController {
 	        BeanUtils.copyProperties(parkingSpotDto, parkingSpotModel);
 	        parkingSpotModel.setId(parkingSpotModelOptional.get().getId());
 	        parkingSpotModel.setRegistrationDate(parkingSpotModelOptional.get().getRegistrationDate());
-	        return ResponseEntity.status(HttpStatus.OK).body(service.save(parkingSpotModel));
+	        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(service.save(parkingSpotModel));
 		}
 	
 	
